d8063e128e5196046b677c5c6c81b9051e6a9612
Michael Verdi <michael.v.verdi@gmail.com>
2019-05-13 15:27:22 -0400

    remove .only from reviews test spec

--- test/reviews-endpoints.spec.js
+++ test/reviews-endpoints.spec.js
@@ -4,7 +4,7 @@ const knex = require('knex');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
-describe.only('Reviews Endpoints', function() {
+describe('Reviews Endpoints', function() {
   let db;
 
   const {
